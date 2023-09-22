48522d3e291df26526a0ba60f75d11e66bef8b5f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-22 23:25:55 +0900

    sliderの値を変化させる

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 'use strict'
 {
 
-  
+  const slider = document.getElementById('slider');
+
+  slider.addEventListener('input', () => {
+      const passwordLength = document.getElementById('password-length');
+      passwordLength.textContent = slider.value;
+  });
 
 
 }
